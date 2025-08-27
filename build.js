82b7c24c810745c2727625a189021447e70c37fc
Dare <darius.emrani@gmail.com>
2025-08-26 22:46:27 -0700

    Move site password to environment variable for security

--- build.js
+++ build.js
@@ -4,9 +4,12 @@ require('dotenv').config();
 // Read the template HTML file
 const html = fs.readFileSync('index.html', 'utf8');
 
-// Replace environment variable placeholder
+// Replace environment variable placeholders
 const windyApiKey = process.env.WINDY_API_KEY || '';
-const processedHtml = html.replace(/\{\{WINDY_API_KEY\}\}/g, windyApiKey);
+const sitePassword = process.env.SITE_PASSWORD || 'dare2fly';
+
+let processedHtml = html.replace(/\{\{WINDY_API_KEY\}\}/g, windyApiKey);
+processedHtml = processedHtml.replace(/\{\{SITE_PASSWORD\}\}/g, sitePassword);
 
 // Write the processed file to dist/index.html
 if (!fs.existsSync('dist')) {
